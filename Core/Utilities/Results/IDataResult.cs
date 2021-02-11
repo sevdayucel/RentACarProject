aec26e662bf75936ab237210593da4cce32167bb
svdycl2858@gmail.com <svdycl2858@gmail.com>
2021-02-12 00:59:36 +0300

    Core katmanı Results yapılandırması eklendi.

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResultIDataResult<T> : IResult
+    {
+        T Data { get; }
+    }
+    
+}
